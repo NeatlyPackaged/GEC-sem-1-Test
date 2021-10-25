2600360f9eba53ffea7c4995658adde403d67daf
NeatlyPackaged <92084795+NeatlyPackaged@users.noreply.github.com>
2021-10-25 13:23:32 +0100

    week changes

---
+++ Week1/Program6_area/Program6_area/Program6_area.cpp
@@ -0,0 +1,16 @@
+// Program6_area.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int width = 15;
+    int height = 25;
+
+    int area = width*height;
+
+    cout << "The area of a " << width << " by " << height << " rectangle is " << area << endl;
+}
+
+
