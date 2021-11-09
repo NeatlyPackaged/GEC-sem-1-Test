597d8de31a588a99cec50ba70d359f5bc492aecd
NeatlyPackaged <92084795+NeatlyPackaged@users.noreply.github.com>
2021-11-09 21:10:22 +0000

    Submission date

--- Week4/Program 15_Starry Output/Program 15_Starry Output.cpp
+++ Week4/Program 15_Starry Output/Program 15_Starry Output.cpp
@@ -6,9 +6,45 @@ using namespace std;
 int main()
 {
 
-	typedef int inputNumber;
-	string yNo;
+	char starDigit = '*';
+	int inputNumber = 0;
 
+	while (true)
+	{
+		cout << "Enter a number between 1 and 10" << endl;
+		cin >> inputNumber;
+
+		string out;
+		for (int i = 0; i < inputNumber; i++)
+		{
+			out += starDigit;
+			cout << out << endl;
+		}
+
+		while (true)
+		{
+			char endOfDialogue;
+			cout << "Would you like to have another go?" << endl;
+			cin >> endOfDialogue;
+
+			if (endOfDialogue == 'y')
+			{
+				cout << "Reloading" << endl;
+				break;
+
+			}
+			else if (endOfDialogue == 'n')
+			{
+				cout << "Farewell" << endl;
+				return 0;
+
+			}
+			else
+			{
+				cout << "Press Y/N to restart or stop" << endl;
+			}
+		}
+	}
 	
 
 	
